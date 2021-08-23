88e47826acbb14417e5cc4f05869a4f49b78341d
arpitha-beltech <78195855+arpitha-beltech@users.noreply.github.com>
2021-08-23 23:24:21 +0530

    FE Changes

--- js/static.js
+++ js/static.js
@@ -278,4 +278,7 @@ $(document).ready(function () {
        $("#sc-1").css("display", "none");
        $("#pt-1").css("display", "none");
     });
+  
+  $('.counter-count').each(function(){$(this).prop('Counter',0).animate({Counter:$(this).text()},{duration:2000,easing:'swing',step:function(now){$(this).text(Math.ceil(now))}})})
+
 });
